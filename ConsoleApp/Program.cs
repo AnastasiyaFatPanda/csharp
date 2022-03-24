d17798b96c0940c12e57297ac48953011c783241
Anastasiya Kakura <AKakura@gomel.iba.by>
2022-03-24 15:52:50 +0300

    task2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,83 +1,92 @@
 ﻿namespace ConsoleApplication
 {
-    public class Mark
-    {
-        public string subject;
-        public int grade;
+  public class Mark
+  {
+    public string subject;
+    public int grade;
 
-        public Mark(string subject, int grade)
-        {
-            this.subject = subject;
-            this.grade = grade;
-        }
+    public Mark(string subject) : this(subject, 0) { }
+
+    public Mark(string subject, int grade)
+    {
+      this.subject = subject;
+      this.grade = grade;
+    }
+  }
+  public enum Gender { Male, Female }
+  public class Student
+  {
+    private string fio;
+    private int age;
+    public Gender gender;
+    private Mark[] marks;
+
+    public Student(string fio, int age, Mark[] marks) : this(fio, Gender.Male, age, marks) { }
+
+    public Student(string fio, Gender gender, int age, Mark[] marks)
+    {
+      this.fio = fio;
+      this.gender = gender;
+      this.age = age;
+      this.marks = marks;
     }
 
-    public class Student
+    public void GetInfo()
     {
-        private string fio;
-        private int age;
-        private Mark[] marks;
+      string grades = "";
 
+      foreach (var mark in marks)
+      {
+        grades += $"{mark.subject} - {mark.grade}; ";
+      }
 
-        public Student(string fio, int age, Mark[] marks)
-        {
-            this.fio = fio;
-            this.age = age;
-            this.marks = marks;
-        }
+      Console.WriteLine($"FIO: {this.fio}, " + $"Gender: {this.gender}, " + $"Age: {this.age}, " + $"Marks: {grades} ");
+    }
+    public float GetAvgMark()
+    {
+      if (marks.Length == 0)
+      {
+        Console.WriteLine("No marks");
+        return 0;
+      }
 
-        public void GetInfo()
-        {
-            string grades = "";
+      var sum = 0;
+      for (int i = 0; i < marks.Length; i++)
+      {
+        sum += marks[i].grade;
+      }
 
-            foreach (var mark in marks)
-            {
-                grades += $"{mark.subject} - {mark.grade}; ";
-            }
-
-            Console.WriteLine($"FIO: {this.fio}, " + $"Age: {this.age}, " + $"Marks: {grades} ");
-        }
-        public float GetAvgMark()
-        {
-            if (this.marks.Length == 0)
-            {
-                Console.WriteLine("No marks");
-                return 0;
-            }
-            int sum = 0;
-            for (int i = 0; i < this.marks.Length; i++)
-            {
-                sum += this.marks[i].grade;
-            }
-
-            Console.WriteLine($"Average mark is {sum / marks.Length}");
-            return sum / this.marks.Length;
-        }
-
-        public void ResetAllMarks()
-        {
-            Console.WriteLine("Delete all marks");
-            for (int i = 0; i < this.marks.Length; i++)
-            {
-                this.marks[i].grade = 0;
-            }
-
-        }
+      Console.WriteLine($"Average mark is {sum / marks.Length} for {fio}");
+      return sum / marks.Length;
     }
 
-    class Program
+    public void ResetAllMarks(int resetMark = 0)
     {
-        static void Main()
-        {
-            Student student = new Student("Ivanov Ivan Ivanovich", 24, new Mark[] { new Mark("math", 10), new Mark("chemistry", 7), new Mark("english", 8) });
+      Console.WriteLine($"Reset all marks to {resetMark} for {fio}");
+      for (int i = 0; i < marks.Length; i++)
+      {
+        marks[i].grade = resetMark;
+      }
 
-            student.GetInfo();
-            student.GetAvgMark();
-            student.ResetAllMarks();
-            student.GetInfo();
-            student.GetAvgMark();
-            Console.ReadKey();
-        }
     }
+  }
+
+  class Program
+  {
+    static void Main()
+    {
+      Student student = new Student("Ivanov Ivan Ivanovich", 24, new Mark[] { new Mark("math"), new Mark("chemistry", 9), new Mark("english", 8) });
+      Student student2 = new Student("Ivanova Olha Ivanovna", Gender.Female, 20, new Mark[] { new Mark("math"), new Mark("chemistry", 4), new Mark("english", 8) });
+
+      student.GetInfo();
+      student.GetAvgMark();
+      student.ResetAllMarks(8);
+      student.GetInfo();
+      student2.ResetAllMarks();
+      student2.GetInfo();
+      student2.GetAvgMark();
+      Console.ReadKey();
+    }
+  }
 
 }
