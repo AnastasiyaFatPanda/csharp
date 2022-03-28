cdbd75eda8a1374376ab6224d9f3dfbd35fb4402
Anastasiya Kakura <AKakura@gomel.iba.by>
2022-03-28 18:17:27 +0300

    task3

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,21 +1,50 @@
-﻿namespace ConsoleApplication
+﻿class Program
 {
-  class Program
+  static void Main()
   {
-    static void Main()
-    {
-      Student student = new Student("Ivanov Ivan Ivanovich", 24, new Mark[] { new Mark("math"), new Mark("chemistry", 9), new Mark("english", 8) });
-      Student student2 = new Student("Ivanova Olha Ivanovna", Gender.Female, 20, new Mark[] { new Mark("math"), new Mark("chemistry", 4), new Mark("english", 8) });
+    Employee[] employees = {
+      new Employee("Ivanov Ivan", new DateOnly(1995, 6, 6)),
+      new Employee("Krav Kristina", Gender.Female, new DateOnly(1985, 11, 29), new DateOnly(2020, 1, 10), 1500),
+      new Employee("Grachova Iryna", Gender.Female, new DateOnly(1995, 12, 6)),
+      new Employee("Som Aleksandra", Gender.Female, new DateOnly(1998, 7, 23), new DateOnly(2020, 1, 10), 1500.01),
+      new Employee("Gorkiy Maksim", Gender.Male, new DateOnly(1985, 11, 29), new DateOnly(1999, 3, 15), 5500.75),
+      new Employee("Pushkin Alexandr", Gender.Male, new DateOnly(1977, 10, 9), new DateOnly(2000, 12, 1), 3500.8),
+    };
 
-      student.GetInfo();
-      student.GetAvgMark();
-      student.ResetAllMarks(8);
-      student.GetInfo();
-      student2.ResetAllMarks();
-      student2.GetInfo();
-      student2.GetAvgMark();
-      Console.ReadKey();
+
+    foreach (Employee employee in SortEmployees(employees, SortBy.Salary))
+      employee.GetInfo();
+
+    foreach (Employee employee in SortEmployees(employees, SortBy.Name))
+      employee.GetInfo();
+
+    foreach (Employee employee in SortEmployees(employees, SortBy.DayOfBirth))
+      employee.GetInfo();
+
+    Console.ReadKey();
+
+    static Employee[] SortEmployees(Employee[] arr, SortBy sortBy)
+    {
+      switch (sortBy)
+      {
+        case SortBy.Salary:
+          Console.ForegroundColor = ConsoleColor.Green;
+          Console.WriteLine("Sort by Salary: \n");
+          Array.Sort(arr, (Employee x, Employee y) => x.salary.CompareTo(y.salary));
+          return arr;
+        case SortBy.Name:
+          Console.ForegroundColor = ConsoleColor.Cyan;
+          Console.WriteLine("\n\nSort by Name: \n");
+          Array.Sort(arr, (Employee x, Employee y) => x.fio.CompareTo(y.fio));
+          return arr;
+        case SortBy.DayOfBirth:
+          Console.ForegroundColor = ConsoleColor.Magenta;
+          Console.WriteLine("\n\nSort by DayOfBirth: \n");
+          Array.Sort(arr, (Employee x, Employee y) => x.dayOfBirth.CompareTo(y.dayOfBirth));
+          return arr;
+        default:
+          return arr;
+      }
     }
   }
-
 }
