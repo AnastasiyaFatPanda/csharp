03a8bb46da51b5558e9277134a0803cc280615ad
Анастасия Какура <kakuraanastasia@gmail.com>
2022-06-07 09:25:58 +0300

    task7_formatting

--- ConsoleApp/Human.cs
+++ ConsoleApp/Human.cs
@@ -2,98 +2,100 @@
 
 namespace ConsoleApp
 {
-    public enum Gender { Male, Female }
-    public class Human
+  public enum Gender { Male, Female }
+  public class Human
+  {
+    private string name;
+    private int age;
+    public bool testFieldForTypeInfo;
+    public string Name
     {
-        private string name;
-        private int age;
-        public int fieldForTypeInfo;
-        public string Name
+      get { return name; }
+      set { name = value; }
+    }
+    public int Age
+    {
+      get { return age; }
+      set
+      {
+        if (value > 0 && value < 100)
         {
-            get { return name; }
-            set { name = value; }
+          age = value;
         }
-        public int Age
+        else
         {
-            get { return age; }
-            set
-            {
-                if (value > 0 && value < 100)
-                {
-                    age = value;
-                }
-                else
-                {
-                    throw new Exception("You cannot set age less than 0 and greater than 100;");
-                }
-            }
+          throw new Exception("You cannot set age less than 0 and greater than 100;");
         }
-        public virtual Gender Gender { get; }
+      }
+    }
+    public virtual Gender Gender { get; }
 
-        public Human(string name = "Noname", int age = 1, Gender gender = Gender.Male)
-        {
-            Name = name;
-            Age = age;
-            Gender = gender;
-        }
-
-        public virtual void WhoAmI()
-        {
-            Console.WriteLine("I'm a human");
-        }
-        public void GetInfo()
-        {
-            string info = "";
-            foreach (PropertyInfo prop in this.GetType().GetProperties()) {
-                info += $"{prop.Name}: {prop.GetValue(this, null)}; ";
-            }
-            Console.WriteLine(info);
-        }
+    public Human(string name = "Noname", int age = 1, Gender gender = Gender.Male)
+    {
+      Name = name;
+      Age = age;
+      Gender = gender;
     }
 
-    public class Boy : Human
+    public virtual void WhoAmI()
     {
-        public string SportClub { get; set; }
-        public override Gender Gender
-        {
-            get => Gender.Male;
-        }
+      Console.WriteLine("I'm a human");
+    }
+    public void GetInfo()
+    {
+      string info = "";
+      foreach (PropertyInfo prop in this.GetType().GetProperties())
+      {
+        info += $"{prop.Name}: {prop.GetValue(this, null)}; ";
+      }
+      Console.WriteLine(info);
+    }
+  }
 
-        public Boy(string name, int age, string sportClub = "No sport clubs") : base(name, age, Gender.Male)
-        {
-            SportClub = sportClub;
-        }
-        public override void WhoAmI()
-        {
-            Console.WriteLine("I'm a boy");
-        }
-
-        public void PlayFootbal()
-        {
-            Console.WriteLine("Boy is playing football");
-        }
+  public class Boy : Human
+  {
+    public string SportClub { get; set; }
+    public override Gender Gender
+    {
+      get => Gender.Male;
     }
 
-    public class Girl : Human
+    public Boy(string name, int age, string sportClub = "No sport clubs") : base(name, age, Gender.Male)
     {
-        public string Adress { get; set; }
-        public override Gender Gender
-        {
-            get => Gender.Female;
-        }
-        public Girl(string name, int age, string address) : base(name, age, Gender.Female)
-        {
-            Adress = address;
-        }
-        public override void WhoAmI()
-        {
-            Console.WriteLine("I'm a girl");
-        }
-
-        public void PlayPiano()
-        {
-            Console.WriteLine("Girl is playing the piano");
-        }
-
+      SportClub = sportClub;
     }
+    public override void WhoAmI()
+    {
+      Console.WriteLine("I'm a boy");
+    }
+
+    public void PlayFootbal()
+    {
+      Console.WriteLine("Boy is playing football");
+    }
+  }
+
+  public class Girl : Human
+  {
+    public string Adress { get; set; }
+    public bool testFieldForTypeInfoGirl;
+    public override Gender Gender
+    {
+      get => Gender.Female;
+    }
+    public Girl(string name, int age, string address) : base(name, age, Gender.Female)
+    {
+      Adress = address;
+    }
+    public override void WhoAmI()
+    {
+      Console.WriteLine("I'm a girl");
+    }
+
+    public void PlayPiano()
+    {
+      Console.WriteLine("Girl is playing the piano");
+    }
+
+  }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,13 +1,13 @@
 ﻿class Program
 {
-    static void Main()
-    {
-        Human human = new Human();
-        Girl girl = new Girl("Anna", 12, "Sovetskaia 44, kv. 32");
-        Boy boy = new Boy("Petr", 14);
-        Reflection.TypeInfo<Human>(human);
-        Reflection.TypeInfo<Girl>(girl);
-        Reflection.TypeInfo<Boy>(boy);
-    }
+  static void Main()
+  {
+    Human human = new Human();
+    Girl girl = new Girl("Anna", 12, "Sovetskaia 44, kv. 32");
+    Boy boy = new Boy("Petr", 14);
+    Reflection.TypeInfo<Human>(human);
+    Reflection.TypeInfo<Girl>(girl);
+    Reflection.TypeInfo<Boy>(boy);
+  }
 
 }
